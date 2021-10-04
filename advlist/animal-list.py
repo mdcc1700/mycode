d4cfb989114ca1cc8cb653a9ca44a633821a9673
Michael Ding <mdcc1700@users.noreply.github.com>
2021-10-04 21:54:58 +0000

    About complex lists

---
+++ advlist/animal-list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+def main():
+
+    # create list of amimal called animal1
+    animal1 = ["Fox", "Fly", "Ant", "Bee", "Cod", "Cat", "Dog", "Yak", "Cow", "Hen", "Koi", "Hog", "Jay", "Kit"]
+
+    #display animal1
+    print(animal1)
+
+    #display amimal1[2] which should list Ant
+    print(animal1[2])
+
+main()
