0d6804208b3bd9a81582c34c3b4949c617e11cb3
Michael Ding <mdcc1700@users.noreply.github.com>
2021-10-06 22:00:38 +0000

    Added line count with -l

---
+++ python_basics/for-and-while-loop.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# for-loop
+forloop = [1,2,3]
+for x in forloop:
+    print("x: {}".format(x))
+                                                                                        
+
+# while-loop
+y = 0
+while y < 5:
+    print("y: {}".format(y))
+    y += 1 # same as y = y+1
